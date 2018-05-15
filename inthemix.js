5e3964cc22036ef1a18411bb3d4519b8321071d3
michael jones <mw.jones2009@gmail.com>
2018-05-15 19:07:31 -0400

    initial push

---
+++ inthemix.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+  console.log('hey we ready!');
+
+  // $('#first' && '#last').on('input', function (event) {
+  //   console.log('okkkkkk');
+  //   var first = $(this).val();
+  //   if (first === '') {
+  //     event.preventDefault();
+  //     $('#first').addClass('error_message')
+  //   } else if {
+
+  //   } else {
+
+  //   } {
+
+  //   }
+  // })
+
+function validate(event){
+
+}
+
+  //  function validate(event) {
+  //   var firstName = $('#first').val();
+  //   event.preverntDefault();
+  //   $('form').submit(validate);
+  //   console.log('working!!!!');
+
+  // if (firstName === '') {
+  //   event.preventDefault();
+  //   $('#first').addClass('error');
+  // } else {
+  //   ('#first')
+  // .removeClass('error');}
+
+  // $('form').submit(validate);
+
+  // $('form').submit(validate);
+  // console.log('working');
+
+
+
+
+
+
+
+
+})
